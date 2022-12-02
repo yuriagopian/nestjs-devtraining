db689488834622828d0f51f09d63120ebd80feef
yuriagopian <yuri.agopian@hotmail.com>
2022-12-02 14:39:24 -0300

    add simple crud at service

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,26 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { Course } from './entities/course.entity';
 
 @Injectable()
 export class CoursesService {
+    private courses: Course[] = [{
+        id: 1,
+        name: "Nest JS",
+        description: "NestJs fundamentals",
+        tags: ["nest", "js", "inciante"],
+        price: 20
+    }]
+
     findAll() {
-        return `Listagem de cursos`
+        return this.courses;
     }
 
     findOne(id: string) {
-        return `Curso #${id}`;
+        return this.courses.find((course: Course) => course.id === Number(id))
     }
 
-    create(data) {
-        return data;
+    create(createCourseDTO: Course) {
+        return this.courses.push(createCourseDTO);
     }
 
     update(id: string, data) {
-        return {
-            id, data
+        const indexCourse = this.courses.findIndex(c => c.id === Number(id));
+        if (indexCourse) {
+            this.courses[indexCourse] = data
+            return
         }
+        throw new Error(`Course not found`)
     }
 
     delete(id: string) {
-        return `Curso #${id} deleted sucessfully`;
+        const indexCourse = this.courses.findIndex(c => c.id === Number(id));
+        if (indexCourse >= 0) {
+            this.courses.splice(indexCourse, 1)
+            return
+        }
+        throw new Error(`Course not found`)
     }
 }
