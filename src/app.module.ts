0fb0126048ec36627ca569001188468190aaedbb
yuriagopian <yuri.agopian@hotmail.com>
2022-12-05 10:31:22 -0300

    add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { CoursesModule } from './courses/courses.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoursesModule,],
+  imports: [CoursesModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: "localhost",
+    port: "5435",
+    username: "postgres",
+    password: "docker",
+    database: "postgres",
+    autoLoadEntities: true,
+    synchronize: true
+  })],
   controllers: [AppController, UsersController],
   providers: [AppService],
 })
